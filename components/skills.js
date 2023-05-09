f7bf9b23134a3c0136fd970a233547907ac3c409
Prisca <priscaebube@gmail.com>
2023-05-09 23:51:02 +0100

    Added recent projects, Multi-step form and Space Tourism Website

--- components/skills.js
+++ components/skills.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Skills = () => {
   return (
     <div id="skills" className="w-full lg:h-screen p-2">
-      <div className="max-w-[1240px] p-2 lg:p-0 mx-auto flex flex-col justify-center h-full">
+      <div className="max-w-[1240px] p-2 py-16 lg:p-0 mx-auto flex flex-col justify-center h-full">
         <p className="text-xl uppercase tracking-widest text-[#a3320b]">
           Skills
         </p>
